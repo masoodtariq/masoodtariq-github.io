a080c490cf3e785438dce34d502ad551600dfcf3
masoodtariq <masoodtariq075@gmail.com>
2023-07-31 15:43:01 +0500

    Updates

---
+++ precache-manifest.95656eb3d7fa370cef0da27f7c88874e.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de765ba26dd824587ff34bb5d31fb16",
+    "url": "/masoodtariq.github.io/index.html"
+  },
+  {
+    "revision": "009c23665670fa2b9d7f",
+    "url": "/masoodtariq.github.io/static/css/3.1fd892bf.chunk.css"
+  },
+  {
+    "revision": "a9c70c53c5a323966d34",
+    "url": "/masoodtariq.github.io/static/css/main.27d4c1fb.chunk.css"
+  },
+  {
+    "revision": "bcf13f7845be3069a4f8",
+    "url": "/masoodtariq.github.io/static/js/2.e17dfb89.chunk.js"
+  },
+  {
+    "revision": "009c23665670fa2b9d7f",
+    "url": "/masoodtariq.github.io/static/js/3.e9c63d1b.chunk.js"
+  },
+  {
+    "revision": "a9c70c53c5a323966d34",
+    "url": "/masoodtariq.github.io/static/js/main.d063bb21.chunk.js"
+  },
+  {
+    "revision": "cef564f0f51f6764bc14",
+    "url": "/masoodtariq.github.io/static/js/runtime-main.170d704d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/masoodtariq.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/masoodtariq.github.io/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "4a56d34869e994553115b189c4b7dbcd",
+    "url": "/masoodtariq.github.io/static/media/Masood.4a56d348.pdf"
+  },
+  {
+    "revision": "2e23bf6d399672f55044dd863af84e75",
+    "url": "/masoodtariq.github.io/static/media/MasoodPic.2e23bf6d.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/masoodtariq.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0444d8df1b78291933dae7165735993a",
+    "url": "/masoodtariq.github.io/static/media/UNICEF-logo.0444d8df.png"
+  },
+  {
+    "revision": "dd26271d8391ad9fe4e1648888dec6e4",
+    "url": "/masoodtariq.github.io/static/media/acmnust.dd26271d.png"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/masoodtariq.github.io/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "3f987fe00dbd1b29cb9a38cf69e70a5b",
+    "url": "/masoodtariq.github.io/static/media/afiniti-logo.3f987fe0.png"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/masoodtariq.github.io/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "47004c68af2e89cea05b17ee5db0d980",
+    "url": "/masoodtariq.github.io/static/media/aws-logo.47004c68.png"
+  },
+  {
+    "revision": "6bb0fcd0d3f00077892b74ffebad764f",
+    "url": "/masoodtariq.github.io/static/media/awsblack.6bb0fcd0.png"
+  },
+  {
+    "revision": "c6d4666f09295045c7942d96c15f4ecb",
+    "url": "/masoodtariq.github.io/static/media/awseducate.c6d4666f.png"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/masoodtariq.github.io/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/masoodtariq.github.io/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "/masoodtariq.github.io/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/masoodtariq.github.io/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "c6fc67b35b4741ea151be5a74fa655b5",
+    "url": "/masoodtariq.github.io/static/media/coursera-Logo.c6fc67b3.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/masoodtariq.github.io/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/masoodtariq.github.io/static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/masoodtariq.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f65ee6f64b46db0ae0f59279ea7853a8",
+    "url": "/masoodtariq.github.io/static/media/dsc_logo.f65ee6f6.png"
+  },
+  {
+    "revision": "90d9e48085c82b8f63dbf941625eebab",
+    "url": "/masoodtariq.github.io/static/media/dsclogo.90d9e480.png"
+  },
+  {
+    "revision": "ac613bb36c2536786174a7f5855e2f90",
+    "url": "/masoodtariq.github.io/static/media/dscnust-logo.ac613bb3.png"
+  },
+  {
+    "revision": "e7a2b385860e7e4ad34d4213a325046f",
+    "url": "/masoodtariq.github.io/static/media/dscnust-short.e7a2b385.png"
+  },
+  {
+    "revision": "22c88b5efae4056a250d443dc4815a02",
+    "url": "/masoodtariq.github.io/static/media/dscnust-vertical.22c88b5e.png"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/masoodtariq.github.io/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "c773d3d44589c63cc95215c71ba599e2",
+    "url": "/masoodtariq.github.io/static/media/error.c773d3d4.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/masoodtariq.github.io/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/masoodtariq.github.io/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/masoodtariq.github.io/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/masoodtariq.github.io/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/masoodtariq.github.io/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/masoodtariq.github.io/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/masoodtariq.github.io/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/masoodtariq.github.io/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/masoodtariq.github.io/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/masoodtariq.github.io/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/masoodtariq.github.io/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/masoodtariq.github.io/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/masoodtariq.github.io/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/masoodtariq.github.io/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/masoodtariq.github.io/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/masoodtariq.github.io/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "ace8e508b9dbad19515670ba55dbb9ca",
+    "url": "/masoodtariq.github.io/static/media/fatima-group.ace8e508.png"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/masoodtariq.github.io/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "5bdb60b8a98d6cba203cdc9d5cbc8ef9",
+    "url": "/masoodtariq.github.io/static/media/fiverr.5bdb60b8.png"
+  },
+  {
+    "revision": "9ae3ae5d66d089b4ac830be3ce52edc6",
+    "url": "/masoodtariq.github.io/static/media/friendshome.9ae3ae5d.png"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/masoodtariq.github.io/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/masoodtariq.github.io/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/masoodtariq.github.io/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/masoodtariq.github.io/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/masoodtariq.github.io/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "549f90324f0b177180c7e1dc13f5fdbb",
+    "url": "/masoodtariq.github.io/static/media/huawei.549f9032.png"
+  },
+  {
+    "revision": "91a9589b78add44d11df61b9a95e21f6",
+    "url": "/masoodtariq.github.io/static/media/ibm_logo.91a9589b.png"
+  },
+  {
+    "revision": "c350d7996596e9909fe72c0938cc9ee5",
+    "url": "/masoodtariq.github.io/static/media/iiitk_logo.c350d799.png"
+  },
+  {
+    "revision": "a03a4dac5dadcfdf3bd750f385ed9e68",
+    "url": "/masoodtariq.github.io/static/media/infinity-gif.a03a4dac.svg"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/masoodtariq.github.io/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/masoodtariq.github.io/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "4ba886bafb9ff0a5b71b800c47f45d90",
+    "url": "/masoodtariq.github.io/static/media/legato_logo.4ba886ba.png"
+  },
+  {
+    "revision": "e395b805b2b27cdcf6c9fa9dc0aeeda0",
+    "url": "/masoodtariq.github.io/static/media/macquarie.e395b805.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/masoodtariq.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0b212d38e34827014c595d35229e961d",
+    "url": "/masoodtariq.github.io/static/media/mcn-logo.0b212d38.png"
+  },
+  {
+    "revision": "268edbf8496f88d136fc1d0df63be865",
+    "url": "/masoodtariq.github.io/static/media/mk.268edbf8.svg"
+  },
+  {
+    "revision": "db6c1c0dddd4c868e00a3cffa1078e49",
+    "url": "/masoodtariq.github.io/static/media/muffito_logo.db6c1c0d.png"
+  },
+  {
+    "revision": "cd7077f7a08bffdb11e13093424e13fd",
+    "url": "/masoodtariq.github.io/static/media/nptel_logo.cd7077f7.png"
+  },
+  {
+    "revision": "4639374a7e51d888ddf0a460d5b0891a",
+    "url": "/masoodtariq.github.io/static/media/nust-logo.4639374a.png"
+  },
+  {
+    "revision": "4a82dc8538a75434a64a189da7237b2a",
+    "url": "/masoodtariq.github.io/static/media/orientation of nust.4a82dc85.png"
+  },
+  {
+    "revision": "eba8d0be1dbc025de6581c6788ea42a7",
+    "url": "/masoodtariq.github.io/static/media/pgclogo.eba8d0be.png"
+  },
+  {
+    "revision": "6e7dc5ad476dd2d57600bca9521f3a5d",
+    "url": "/masoodtariq.github.io/static/media/portfolio.6e7dc5ad.gif"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/masoodtariq.github.io/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/masoodtariq.github.io/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "ffa9e58687cf88c9bcfb214b458c847a",
+    "url": "/masoodtariq.github.io/static/media/ptcl.ffa9e586.png"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/masoodtariq.github.io/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "bf236ba230db3b5da58cb8542c6d760c",
+    "url": "/masoodtariq.github.io/static/media/quantaleap.bf236ba2.png"
+  },
+  {
+    "revision": "b9210e04e48ad03b6227fda111db7489",
+    "url": "/masoodtariq.github.io/static/media/romi-logo.b9210e04.png"
+  },
+  {
+    "revision": "33533403dcbce1d5ad7ec0e37fb67606",
+    "url": "/masoodtariq.github.io/static/media/royalcyber.33533403.png"
+  },
+  {
+    "revision": "5a31f2370c8081282d766807cf94acd9",
+    "url": "/masoodtariq.github.io/static/media/shiftcrowd.5a31f237.png"
+  },
+  {
+    "revision": "cef3ae2a288ebee3bff8511504e64bc8",
+    "url": "/masoodtariq.github.io/static/media/srp-logo.cef3ae2a.png"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/masoodtariq.github.io/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/masoodtariq.github.io/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/masoodtariq.github.io/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "b369e5d263eaa476e2a77906a4642d25",
+    "url": "/masoodtariq.github.io/static/media/telenor.b369e5d2.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/masoodtariq.github.io/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
